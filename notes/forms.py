57015bd12455835a2d525a727b3ced178e1c85d6
Satej <Satej>
2023-03-05 12:40:38 +0000

    Display error

--- notes/forms.py
+++ notes/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Notes
 
@@ -6,3 +7,9 @@ class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
         fields = ('title', 'text')
+    
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if 'Django' not in title:
+            raise ValidationError('We only accept notes about Django!')
+        return title
